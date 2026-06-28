47e25fb23210542ba93fe2ea7a311d19791db031
Nethum <nethumnenula2004@gmail.com>
2026-06-28 19:31:29 +0530

    Added Basic elements to Projects Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css'
-import Header from './Header/Header.jsx'
-import Hero from './Hero/Hero.jsx'
-import About from './About/About.jsx';
-import Stack from './Stack/Stack.jsx';
-import Projects from './Projects/Projects.jsx';
+import "./App.css";
+import Header from "./Header/Header.jsx";
+import Hero from "./Hero/Hero.jsx";
+import About from "./About/About.jsx";
+import Stack from "./Stack/Stack.jsx";
+import Projects from "./Projects/Projects.jsx";
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Hero/>
-      <About/>
-      <Stack/>
+      <Header />
+      <Hero />
+      <About />
+      <Stack />
       <Projects />
-    </div>  
+    </div>
   );
 }
 
-export default App
+export default App;
